c02775f50c4d18ad93c73ac7ce394dc055c52d00
Xor Boole <mcyoung@mit.edu>
2015-03-08 23:26:42 -0400

    COMPRESSSSS

--- src/main/java/com/xorinc/bf/Main.scala
+++ src/main/java/com/xorinc/bf/Main.scala
@@ -4,10 +4,6 @@ import java.io.{Console => _, _}
 
 object Main {
 
-  def exit(i: Int): Nothing = {
-    System.exit(i); ??? // keeping the compiler happy
-  }
-
   def main(args: Array[String]): Unit = {
     case class Conf(in: Reader = null, inAsString: Boolean = false, exitCode: Boolean = false, verbose: Boolean = false)
     val parser = new scopt.OptionParser[Conf]("brainscala") {
@@ -40,78 +36,58 @@ object Main {
       }
 
     val exitVal = prog.run()
-
     if(opts.exitCode) exit(exitVal)
   }
+  // keeping the compiler happy
+  def exit(i: Int): Nothing = { System.exit(i); ??? }
 }
 
 object Interpreter {
 
   case class ParseException(message: String) extends Exception(message)
 
-  val legals = "+-<>.,[]".toCharArray.toSet
+  private val legals = "+-<>.,[]".toCharArray.toSet
+  private val nonLoopChars = legals - '[' - ']'
 
-  sealed trait Instruction {
-
-    def apply(): Unit
+  private object nonLoop { def unapply(c: Char): Option[Char] = if(nonLoopChars(c)) Some(c) else None }
+  private def instruction(c: Char): Instruction = c match {
+    case '+' => Plus
+    case '-' => Minus
+    case '>' => Right
+    case '<' => Left
+    case '.' => Write
+    case ',' => Read
   }
-  case object Plus extends Instruction {
 
-    override def apply(): Unit = Tape.plus()
-  }
-  case object Minus extends Instruction {
+  sealed trait  Instruction               { def apply(): Unit }
+  case   object Plus  extends Instruction { override def apply(): Unit = Tape.plus() }
+  case   object Minus extends Instruction { override def apply(): Unit = Tape.minus() }
+  case   object Right extends Instruction { override def apply(): Unit = Tape.right() }
+  case   object Left  extends Instruction { override def apply(): Unit = Tape.left() }
+  case   object Write extends Instruction { override def apply(): Unit = Console.out.print(Tape()) }
+  case   object Read  extends Instruction { override def apply(): Unit = Tape() = Console.in.read().toChar }
 
-    override def apply(): Unit = Tape.minus()
-  }
-  case object Right extends Instruction {
-
-    override def apply(): Unit = Tape.right()
-  }
-  case object Left extends Instruction {
-
-    override def apply(): Unit = Tape.left()
-  }
-  case object Write extends Instruction {
-
-    override def apply(): Unit = Console.out.print(Tape())
-  }
-  case object Read extends Instruction {
-
-    override def apply(): Unit = Tape() = Console.in.read().toChar
-  }
-  sealed case class Loop(ops: List[Instruction]) extends Instruction {
-
-    override def apply(): Unit =
-      do ops.foreach(_())
-      while (Tape() != 0)
+  final case class Loop(ops: List[Instruction]) extends Instruction {
+    override def apply(): Unit = do ops.foreach(_()) while (Tape() != 0)
   }
 
   case class Program(ops: List[Instruction]) {
-
-    def run(): Int = {
-      ops.foreach(_())
-      Tape()
-    }
+    def run(): Int = { ops.foreach(_()); Tape() }
   }
 
   def compile(s: String) = {
-    if(
+    if (
       s.iterator.filter(s => s == '[' || s == ']').map {
         case '[' => 1
         case ']' => -1
       }.sum != 0
-    ) throw ParseException("Unbalanced loop delimiters!") // TODO
+    ) throw ParseException("Unbalanced loop delimiters!")
 
     def doCompile(iter: Iterator[Char], list: List[Instruction] = Nil): Program = {
       def enterLoop(list: List[Instruction] = Nil): Loop = {
           enterLoop(
             (iter.next() match {
-              case '+' => Plus
-              case '-' => Minus
-              case '>' => Right
-              case '<' => Left
-              case '.' => Write
-              case ',' => Read
+              case nonLoop(c) => instruction(c)
               case '[' => enterLoop()
               case ']' => return Loop(list.reverse)
             }) :: list
@@ -120,12 +96,7 @@ object Interpreter {
       if (iter.hasNext)
         doCompile(iter,
           (iter.next() match {
-            case '+' => Plus
-            case '-' => Minus
-            case '>' => Right
-            case '<' => Left
-            case '.' => Write
-            case ',' => Read
+            case nonLoop(c) => instruction(c)
             case '[' => enterLoop()
           }) :: list
         )
@@ -137,13 +108,9 @@ object Interpreter {
 
 object Tape {
 
-  import collection.mutable.ArrayBuffer
-
   private val default = '\0'
-
-  private val positive = ArrayBuffer.fill(64)(default)
-  private val negative = ArrayBuffer.fill(63)(default)
-
+  private val positive = collection.mutable.ArrayBuffer.fill(64)(default)
+  private val negative = collection.mutable.ArrayBuffer.fill(63)(default)
   private var _pointer = 0
   def pointer: Int = _pointer
 
@@ -166,7 +133,6 @@ object Tape {
     else
       negative(-pointer) = (negative(-pointer) + 1).toChar
   }
-
   def minus(): Unit = {
     if(pointer >= 0)
       positive(pointer) = (positive(pointer) - 1).toChar
